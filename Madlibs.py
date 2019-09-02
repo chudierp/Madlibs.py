c0c54ec564e9286a73f2f173775953361ef098f8
Chudier <chudierp@gmail.com>
2019-09-02 12:45:22 -0700

    Added the array for parts of speeach

--- Madlibs.py
+++ Madlibs.py
@@ -1,4 +1,19 @@
-Print("Agent Black Hawk you are currently on a top secret mission.")
-Print("where you will be using the identity of a QUIRKY zoolist.")
-Print("You work specifically with the MONKEYS")
-Print("in the JUNGLE GYM where you will TEACH them how to FLY PLANES.")
+
+
+print("feed me three nouns")
+nouns = [input(),input(),input()]
+print("feed me two adjectives")
+adjective = [input(),input()]
+print("feed me a verb")
+verbs =[input()]
+
+
+
+
+def show():
+    print(" Agent " + adjective[0] + nouns[0]+"you are currently on a top secret mission")
+    print("where you will be using the identity of a "+ adjective[1]+ " zoologist.")
+    print("You work specifically with the " + nouns[1])
+    print(" in the " + nouns[2]+ " where you will teach them how to "+verbs[0])
+
+show()
