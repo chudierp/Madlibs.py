234ea4348ffd73547ced1a3e44067ecd4ec93372
Chudier <chudierp@gmail.com>
2019-09-01 21:37:44 -0700

    I printed my Madlib paragraph

---
+++ Madlibs.py
@@ -0,0 +1,4 @@
+Print("Agent Black Hawk you are currently on a top secret mission.")
+Print("where you will be using the identity of a QUIRKY zoolist.")
+Print("You work specifically with the MONKEYS")
+Print("in the JUNGLE GYM where you will TEACH them how to FLY PLANES.")
