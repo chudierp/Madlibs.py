2e8074d8bd76ddaf49d297d4efe5e03d6780bfd8
Chudier <chudierp@gmail.com>
2019-09-03 10:40:18 -0700

    Made sure there were no errors.

--- Madlibs.py
+++ Madlibs.py
@@ -11,7 +11,7 @@ verbs =[input()]
 
 
 def show():
-    print(" Agent " + adjective[0] + nouns[0]+"you are currently on a top secret mission")
+    print("Agent" + adjective[0] + nouns[0]+"you are currently on a top secret mission")
     print("where you will be using the identity of a "+ adjective[1]+ " zoologist.")
     print("You work specifically with the " + nouns[1])
     print(" in the " + nouns[2]+ " where you will teach them how to "+verbs[0])
